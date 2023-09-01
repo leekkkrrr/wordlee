115d603aabe0a4a622d7121ecaeba296fd9f8550
leekkkrrr <136429939+leekkkrrr@users.noreply.github.com>
2023-09-01 16:11:27 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ function appStart() {
   const handleKeydown = (event) => {
     const key = event.key;
     const keyCode = event.keyCode;
-    document.querySelector(".board-block[data-index='00']");
+    const thisBlock = document.querySelector(".board-block[data-index='00']");
+    thisBlock.innerText = key;
   };
 
   window.addEventListener("keydown", handleKeydown);
